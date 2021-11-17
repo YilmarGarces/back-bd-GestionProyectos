578f98a560b18a8c4d19ac1adc1af10583390e48
YilmarGarces <gilmar.garces@gmail.com>
2021-11-17 11:46:48 -0500

    creacion usuarios

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import {Enum_Rol} from "./models/enum";
 const main=async() =>{
     await conectarBD();
     // codigo para crear los usuarios
-    /*await userModel.create({
+    await userModel.create({
         correo:'asdfg@gg.com',
         identificacion:'999999999999',
         nombre:'yilmar',
@@ -15,14 +15,14 @@ const main=async() =>{
         console.log('usuario creado',u);
     }).catch((e=>{
         console.error('error creando el usuario',e);
-    }));*/
+    }));
 
     // codigo para realizar consulta a base de datos y traer todos los usuarios existentes
-    await userModel.find().then(u=>{
+    /*await userModel.find().then(u=>{
         console.log("usuarios",u);
     }).catch(e=>{
         console.error("error obteniendo los usuarios",e);
-    });
+    });*/
     
 }
 

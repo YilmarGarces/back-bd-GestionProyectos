3b7297a4b32a111d27903495193fbb5a6f0f43b3
YilmarGarces <gilmar.garces@gmail.com>
2021-12-03 13:36:18 -0500

    actualizando con front

--- models/usuarios/tipos.js
+++ models/usuarios/tipos.js
@@ -43,8 +43,8 @@ type Mutation{
         apellido:String!
         identificacion:String!
         correo:String!
-        estado:Enum_EstadoUsuario
-        rol:Enum_Rol!
+        estado:Enum_EstadoUsuario!
+        rol:Enum_Rol
     ):Usuario
     eliminarUsuario(_id:String! correo:String):Usuario
     
